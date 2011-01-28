92e7159fcc9c3e062b5c545c98e2871863be02bb
Chris Garrett <z@zworkbench.com>
2011-01-28 18:33:42 -0500

    Fixed memory leak in ABContact

--- Classes/External/ABContactHelper/ABContact.m
+++ Classes/External/ABContactHelper/ABContact.m
@@ -63,7 +63,7 @@
 	ABAddressBookRef addressBook = ABAddressBookCreate();
 	ABRecordRef contactrec = ABAddressBookGetPersonWithRecordID(addressBook, recordID);
 	ABContact *contact = [self contactWithRecord:contactrec];
-	// CFRelease(contactrec); // Thanks Gary Fung
+	CFAutorelease(addressBook);
 	return contact;
 }
 
