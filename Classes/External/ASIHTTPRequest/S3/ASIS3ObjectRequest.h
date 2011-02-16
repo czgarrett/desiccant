a111418a10ac24453fb70eb9014428bc8576ecf0
Chris Garrett <z@zworkbench.com>
2011-02-16 18:45:26 -0500

    Fixed compile error in LLVM

--- Classes/External/ASIHTTPRequest/S3/ASIS3ObjectRequest.h
+++ Classes/External/ASIHTTPRequest/S3/ASIS3ObjectRequest.h
@@ -65,14 +65,7 @@ extern NSString *const ASIS3StorageClassReducedRedundancy;
 
 // Create a PUT request to copy an object from one location to another
 // Clang will complain because it thinks this method should return an object with +1 retain :(
-#ifndef NS_RETURNS_NOT_RETAINED
-#if __has_feature(attribute_ns_returns_not_retained)
-#define NS_RETURNS_NOT_RETAINED __attribute__((ns_returns_not_retained))
-#else
-#define NS_RETURNS_NOT_RETAINED
-#endif
-#endif
-+ (id)COPYRequestFromBucket:(NSString *)sourceBucket key:(NSString *)sourceKey toBucket:(NSString *)bucket key:(NSString *)key NS_RETURNS_NOT_RETAINED;
++ (id)COPYRequestFromBucket:(NSString *)sourceBucket key:(NSString *)sourceKey toBucket:(NSString *)bucket key:(NSString *)key;
 
 // Creates a HEAD request for the object at path
 + (id)HEADRequestWithBucket:(NSString *)bucket key:(NSString *)key;
