2dfd6af20e0a03b3f1f935baf34d1ed1977cc316
Chris Garrett <z@zworkbench.com>
2011-04-21 12:45:52 -0400

    Added NSDateFormatter helper for relative dates.  Untested at this point

---
+++ desiccant/Classes/Zest/Foundation/NSDateFormatter+Zest.h
@@ -0,0 +1,17 @@
+//
+//  NSDateFormatter+Zest.h
+//  desiccant
+//
+//  Created by Christopher Garrett on 4/21/11.
+//  Copyright 2011 ZWorkbench, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSDateFormatter (Zest)
+
+- (NSString *) relativeDateString: (NSDate *) date;
+
+
+@end
